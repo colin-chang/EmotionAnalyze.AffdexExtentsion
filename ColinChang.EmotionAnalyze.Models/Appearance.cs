9daa56f26cca82a727aa849e6659f8833f8a4b7b
Colin Chang <zhangcheng5468@gmail.com>
2019-03-24 15:36:03 +0800

    拆分Model项目

---
+++ ColinChang.EmotionAnalyze.Models/Appearance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColinChang.EmotionAnalyze.Model
+{
+    public class Appearance
+    {
+        public Ethnicity Ethnicity { get; set; }
+        public Age Age { get; set; }
+        public Glasses Glasses { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
